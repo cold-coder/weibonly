88064c717643ddfb67dc94ad4554bbb9ee5c9a8a
Yao Cheng <sz.yaocheng@gmail.com>
2016-06-08 11:43:22 +0800

    scss改写

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+(function(){
+	"use strict";
+	var sass = require("gulp-sass");
+	var gulp = require("gulp");
+
+	gulp.task("sass", function(){
+		return	gulp.src(
+						"main.scss"
+					).pipe(
+						sass().on('error', sass.logError)
+					).pipe(
+					);
+	})
+}())
