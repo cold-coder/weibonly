980a0eee1e39592b9c0260a3936aa3b5b13b2da7
Yao Cheng <sz.yaocheng@gmail.com>
2016-06-13 15:31:06 +0800

    Add autoprofixer and cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 (function(){
 	"use strict";
 	var sass = require("gulp-sass");
+	var autoprefixer = require('gulp-autoprefixer');
+	var cssnano = require('gulp-cssnano');
 	var gulp = require("gulp");
 
 	//编译scss
@@ -10,6 +12,10 @@
 					).pipe(
 						sass().on('error', sass.logError)
 					).pipe(
+						autoprefixer()
+					).pipe(
+						cssnano()
+					).pipe(
 						gulp.dest("./build")
 					);
 	})
@@ -17,8 +23,9 @@
 	//监听文件变化
 	gulp.task("watch", function () {
         gulp.watch("main.scss", ["sass"]); 
-
     });
+
+    gulp.task("dev", ['sass', 'watch']);
 
     
 }())
